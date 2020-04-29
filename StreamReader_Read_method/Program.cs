ecd8e312f228f195e35cbdce7b0eef22883d12d1
임정희 <ynl120721@gmail.com>
2020-04-29 16:58:43 +0900

    MSDN streamReader.Read method 1번예제

---
+++ StreamReader_Read_method/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StreamReader_Read_method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"c:\Temp\MyTest.txt";
+
+            try
+            {
+                if (File.Exists(path))
+                {
+                    File.Delete(path);
+                }
+
+                using (StreamWriter sw = new StreamWriter(path))
+                {
+                    sw.WriteLine("This");
+                    sw.WriteLine("is some text");
+                    sw.WriteLine("to test");
+                    sw.WriteLine("Reading");
+                }
+
+                using (StreamReader sr = new StreamReader(path))
+                {
+                    while (sr.Peek() > 0)
+                    {
+                        Console.WriteLine((char)sr.Read());
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("The process failed : {0}", e.ToString());
+            }
+        }
+    }
+}
---
+++ StreamReader_Read_method/obj/Debug/StreamReader_Read_method.csproj.FileListAbsolute.txt
@@ -0,0 +1,6 @@
+C:\Users\user\Desktop\mscnEx\StreamReader_Read_method\bin\Debug\StreamReader_Read_method.exe.config
+C:\Users\user\Desktop\mscnEx\StreamReader_Read_method\bin\Debug\StreamReader_Read_method.exe
+C:\Users\user\Desktop\mscnEx\StreamReader_Read_method\bin\Debug\StreamReader_Read_method.pdb
+C:\Users\user\Desktop\mscnEx\StreamReader_Read_method\obj\Debug\StreamReader_Read_method.csprojAssemblyReference.cache
+C:\Users\user\Desktop\mscnEx\StreamReader_Read_method\obj\Debug\StreamReader_Read_method.exe
+C:\Users\user\Desktop\mscnEx\StreamReader_Read_method\obj\Debug\StreamReader_Read_method.pdb
